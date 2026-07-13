1d1ae2dd90ba0c929cc991367e78725d0d5509df
Anna Barse <142820538+anna-bars@users.noreply.github.com>
2026-07-13 21:41:21 +0400

    new sol1

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,16 +1,18 @@
+// app.routes.server.ts
 import { RenderMode, ServerRoute } from '@angular/ssr';
-import { BLOGS } from './data/blogs.data'; // կամ Supabase query, ապագայում
+import { BLOGS } from './data/blogs.data';
 
 export const serverRoutes: ServerRoute[] = [
   {
     path: 'blog/:slug',
     renderMode: RenderMode.Prerender,
     getPrerenderParams: async () => {
+      // Այստեղ BLOGS-ը ֆիքսված արրեյ է, որը հասանելի է build-ի ժամանակ
       return BLOGS.map(blog => ({ slug: blog.slug }));
     }
   },
   {
     path: '**',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Prerender // Բոլոր մյուս էջերը նույնպես prerender
   }
 ];
